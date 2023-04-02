60bc22280f851b8c22424f41d6814dfc99cc3608
yellowsnake8 <iwasa.works@gmail.com>
2023-04-02 12:25:01 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict';
 
-function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
-}
+// function fib(n) {
+//     if (n === 0) {
+//         return 0;
+//     } else if (n === 1) {
+//         return 1;
+//     }
+//     return fib(n - 1) + fib(n - 2);
+// }
 
 // n === 2 の時
 // return fib(2 - 1) + fib(2 - 2)
@@ -23,6 +23,19 @@ function fib(n) {
 // return fib(1) + fib(0) + fib(1)
 // return 1 + 0 + 1
 // return 2
+
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
+function fib(n) {
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
+}
 const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(`${i}番目は` + fib(i));
